6a6c8a0b820868a581ed67e1aa581330aed52187
xnovk <krystof.nov@seznam.cz>
2014-09-16 17:29:07 +0200

    Vomit 3  prvni pouzitelna verze  mala hra na zapamatovani cisla

--- GitTest.dpr
+++ GitTest.dpr
@@ -7,10 +7,50 @@ uses
 
 var
   ReadStr: String;
+  RnInt: Integer;
+  Xit: Boolean;
 
+procedure ClrScr;       //Na smazani obrazovky
+var
+ i: integer;
 begin
+ for i:=1 to 100 do
+  writeln('');
+
+end;
+
+
+
+{--------------MAIN-------------------}
+begin
+  Xit:= False;
+
   WriteLn('Hello world!');
-  ReadLn(ReadStr);
-  WriteLn(ReadStr);
-  Sleep(1000);
+  Sleep(2500);
+  ClrScr;
+
+  Repeat
+   begin
+    Randomize;
+    RnInt:= Random(10000);
+    Writeln(IntToStr(RnInt));
+    Sleep(2000);
+    ClrScr;
+    WriteLn('Tell me that number');
+    ReadLn(ReadStr);
+
+    If (ReadStr= IntToStr(RnInt)) then
+     begin
+      WriteLn('Ok');
+      Sleep(1000);
+     end
+    else
+     begin
+       WriteLn('No');
+       Sleep(1000);
+       Xit:= True;
+     end;
+   end;
+  Until
+   Xit;
 end.
